7a7e51f2038abf5bc98e3f89619f3dbed1ca29a1
Aaron Lord <hire@aaron.codes>
2017-07-26 12:29:29 +0100

    Added GotoWebinar::fake()

--- src/Facade/GotoWebinar.php
+++ src/Facade/GotoWebinar.php
@@ -3,13 +3,20 @@
 namespace Slakbal\Citrix\Facade;
 
 use Illuminate\Support\Facades\Facade;
+use Mockery;
 use Slakbal\Citrix\Webinar;
 
 class GotoWebinar extends Facade
 {
-
     protected static function getFacadeAccessor()
     {
         return Webinar::class;
+    }
+
+    public static function fake()
+    {
+        $mock = Mockery::mock(Webinar::class);
+        static::swap($mock);
+        return $mock;
     }
 }
